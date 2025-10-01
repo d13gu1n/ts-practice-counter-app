3abcd29a0d8f4651e54ebda270f8afd43c340ff1
dacevedom <d.acevedom1993@gmail.com>
2025-10-01 19:13:19 -0300

    Added TODO to task.ts

--- task.ts
+++ task.ts
@@ -1,3 +1,5 @@
+// TODO: Install webpack to be able to use compiled TS code on HTML files
+
 const counter = document.getElementById("counter") as HTMLDivElement;
 const incrementBtn = document.getElementById("increment") as HTMLButtonElement;
 const decrementBtn = document.getElementById("decrement") as HTMLButtonElement;
